61d073c1bf005b88f1bebc0dd39c3022a57c6865
unknown <shsim@edu.uwaterloo.ca>
2018-08-09 20:21:39 -0700

    url routes for pkh

--- pubg/pkh/urls.py
+++ pubg/pkh/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'pkh'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), 
     path('matches/', views.matches, name='matches'),
     path('hierarchy/<slug:matchId>', views.hierarchy, name='hierarchy'),
 ]
